a147311178c6d87c38c4919f9d6465958a130d40
shiyongbiao <shiyongbiao@incar100.com>
2018-11-05 16:52:26 +0800

    添加依赖junit-jupiter-engine、junit-platform-surefire-provider

--- src/test/java/com/my/sample/mybatis/SampleMybatisApplicationTests.java
+++ src/test/java/com/my/sample/mybatis/SampleMybatisApplicationTests.java
@@ -9,10 +9,8 @@ import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
